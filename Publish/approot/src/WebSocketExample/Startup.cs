b36cbea70d52f58a38ce074d8952f7c2b8cba969
Константин Дьяченко <konard@me.com>
2015-11-05 21:44:12 +0300

    Source files and published files added.

---
+++ Publish/approot/src/WebSocketExample/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using System.Net.WebSockets;
+using Microsoft.AspNet.WebSockets.Server;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.Http;
+using System.Collections.Generic;
+
+namespace WebSocketExample
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Map("/ws", builder =>
+            {
+                builder.Use(async (context, next) =>
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(webSocket);
+                        return;
+                    }
+                    await next();
+                });
+            });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
---
+++ Publish/web.cmd
@@ -0,0 +1,2 @@
+
+@"%~dp0approot\runtimes\dnx-clr-win-x86.1.0.0-beta6\bin\dnx.exe" --appbase "%~dp0approot\src\WebSocketExample" Microsoft.Framework.ApplicationHost web %*
